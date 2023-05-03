d1a66c5712f6d37451506c181619a4a9860bb2f3
Manuel Lopez <mp.arturolopez@gmail.com>
2023-05-03 10:53:23 -0400

    chapter 6

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux{
-    mux := http.NewServeMux()
+	"github.com/justinas/alice"
+)
 
+func (app *application) routes() http.Handler {
+	mux := http.NewServeMux()
 
-    fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-    return mux
+    standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(mux) 
 }
